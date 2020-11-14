ba14ed9da5bf952b65a49afb3513703b47a9a864
A_Hasebe <akihisahasebe@gmail.com>
2020-11-14 14:59:38 +0900

    first commit

---
+++ src/crop_template.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+import cv2
+
+
+tree = ET.parse('./src/lize_00811_0.xml')
+
+root = tree.getroot()
+
+crop_pos = []
+
+for child in root.find('object').find('bndbox'):
+    crop_pos.append(int(child.text))
+
+img = cv2.imread('./src/lize_00811_0.png')
+
+roi = img[crop_pos[1]:crop_pos[3],crop_pos[0]:crop_pos[2]]
+
+cv2.imwrite('./src/template.png',roi)
